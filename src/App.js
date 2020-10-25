533b146a85efc3f91fb3ff95dba02e66b9b7a3ad
Coalter <coaltergeist@gmail.com>
2020-10-25 17:51:30 -0400

    assignment 1

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ class App extends Component {
     return (
       <div>
         <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes={this.state.dishes} />
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
