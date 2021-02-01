d5cccce4122a988acbdcfb5c11f43c7bb26a7791
coalterio <coalter.putnal@gmail.com>
2021-02-01 16:05:00 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,47 +1,58 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
+//import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        //selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
 
-//   onDishSelect(dishId) {
-//     this.setState({ selectedDish: dishId});
-//   }
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        };
+    }
 
-  render() {
+    //   onDishSelect(dishId) {
+    //     this.setState({ selectedDish: dishId});
+    //   }
 
-    const HomePage = () => {
-        return(
-            <Home />
+    render() {
+
+        const HomePage = () => {
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
         );
-      }
-
-    return (
-      <div>
-        <Header />
-        <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
-          </Switch>
-        <Footer />
-      </div>
-    );
-  }
+    }
 }
 
 export default Main;
