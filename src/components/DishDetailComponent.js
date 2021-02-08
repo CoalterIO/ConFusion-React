eb64dd2d5d236d23b66575d33a34112b2f55983c
coalterio <coalter.putnal@gmail.com>
2021-02-08 15:02:51 -0500

    extra

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -9,8 +9,6 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
-const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class CommentForm extends Component {
 
@@ -28,6 +26,13 @@ class CommentForm extends Component {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
+    }
+
+    handleSubmit = (values) => {
+        console.log('Current State is: ' + JSON.stringify(values));
+        // alert('Current State is: ' + JSON.stringify(values));
+        // event.preventDefault();
+        this.toggleModal();
     }
 
     render() {
